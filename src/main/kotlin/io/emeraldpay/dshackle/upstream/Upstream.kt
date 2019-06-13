748b94fb6cafee210b9cc72bc326a0f0a651926f
Igor Artamonov <igor@artamonov.ru>
2019-06-12 23:52:08 -0400

    solution: show in log when gets an upstream

--- src/main/kotlin/io/emeraldpay/dshackle/upstream/Upstream.kt
+++ src/main/kotlin/io/emeraldpay/dshackle/upstream/Upstream.kt
@@ -3,12 +3,15 @@ package io.emeraldpay.dshackle.upstream
 import io.emeraldpay.grpc.Chain
 import io.infinitape.etherjar.rpc.json.BlockJson
 import io.infinitape.etherjar.rpc.json.TransactionJson
+import org.slf4j.LoggerFactory
 
 class Upstream(
         val chain: Chain,
         val api: EthereumApi,
         private val ethereumWs: EthereumWs? = null
 ) {
+
+    private val log = LoggerFactory.getLogger(Upstream::class.java)
 
     val head: EthereumHead = if (ethereumWs != null) {
         EthereumWsHead(ethereumWs)
@@ -19,6 +22,6 @@ class Upstream(
     }
 
     init {
-
+        log.info("Configured for ${chain.chainName}")
     }
 }
