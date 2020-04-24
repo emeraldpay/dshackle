f827f5a97ab86aa9ea185feb75a5cfe665a8b762
Igor Artamonov <igor@artamonov.ru>
2020-04-24 00:04:10 -0400

    problem: critical bitcoin methods are not enabled by default

--- src/main/kotlin/io/emeraldpay/dshackle/upstream/bitcoin/DefaultBitcoinMethods.kt
+++ src/main/kotlin/io/emeraldpay/dshackle/upstream/bitcoin/DefaultBitcoinMethods.kt
@@ -3,7 +3,12 @@ package io.emeraldpay.dshackle.upstream.bitcoin
 import io.emeraldpay.dshackle.upstream.calls.DirectCallMethods
 
 class DefaultBitcoinMethods : DirectCallMethods(
-        listOf("getbestblockhash", "getblock", "gettransaction")
+        listOf(
+                "getbestblockhash", "getblock", "getblocknumber", "getblockcount",
+                "gettransaction", "getrawtransaction", "gettxout",
+                "getreceivedbyaddress", "listunspent",
+                "getmemorypool"
+        )
 ) {
 
 
