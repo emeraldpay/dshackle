b64d97aa4b7c0eb0ae85d5c82b70c150eacbb06c
Igor Artamonov <igor@artamonov.ru>
2021-09-05 20:07:34 -0400

    working JSON RPC extended metrics

--- src/main/kotlin/io/emeraldpay/dshackle/proxy/ProxyServer.kt
+++ src/main/kotlin/io/emeraldpay/dshackle/proxy/ProxyServer.kt
@@ -18,7 +18,6 @@ package io.emeraldpay.dshackle.proxy
 
 import io.emeraldpay.api.proto.BlockchainOuterClass
 import io.emeraldpay.api.proto.Common
-import io.emeraldpay.dshackle.ChainValue
 import io.emeraldpay.dshackle.Global
 import io.emeraldpay.dshackle.TlsSetup
 import io.emeraldpay.dshackle.config.ProxyConfig
@@ -44,8 +43,13 @@ import reactor.netty.http.server.HttpServer
 import reactor.netty.http.server.HttpServerRequest
 import reactor.netty.http.server.HttpServerResponse
 import reactor.netty.http.server.HttpServerRoutes
+import java.util.*
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.locks.ReentrantReadWriteLock
 import java.util.function.BiFunction
+import kotlin.collections.HashMap
+import kotlin.concurrent.read
+import kotlin.concurrent.write
 
 /**
  * HTTP Proxy Server
@@ -84,6 +88,12 @@ class ProxyServer(
         }
     }
 
+    private val requestMetrics: RequestMetricsFactory = if (Global.metricsExtended) {
+        ExtendedRequestMetrics()
+    } else {
+        StandardRequestMetrics()
+    }
+
     fun start() {
         if (!config.enabled) {
             log.debug("Proxy server is not enabled")
@@ -113,9 +123,21 @@ class ProxyServer(
     }
 
     fun execute(chain: Chain, call: ProxyCall, handler: AccessHandlerHttp.RequestHandler): Publisher<String> {
+        // return empty response for empty request
+        if (call.items.isEmpty()) {
+            return if (call.type == ProxyCall.RpcType.BATCH) {
+                Mono.just("[]")
+            } else {
+                Mono.just("")
+            }
+        }
         val startTime = System.currentTimeMillis()
-        val metrics = RequestMetrics(chain, call.items.get(0).method)
-        metrics.requestMetric.increment()
+        // during the execution we know only ID of the call, we use it to find the origin call and associated metrics
+        val metricById = { id: Int ->
+            call.items.find { it.id == id }?.let { item ->
+                requestMetrics.get(chain, item.method)
+            }
+        }
         val request = BlockchainOuterClass.NativeCallRequest.newBuilder()
                 .setChain(Common.ChainRef.forNumber(chain.id))
                 .addAllItems(call.items)
@@ -124,11 +146,17 @@ class ProxyServer(
         val jsons = nativeCall
                 .nativeCallResult(Mono.just(request))
                 .doOnNext {
-                    handler.onResponse(it)
-                    metrics.callMetric.record(System.currentTimeMillis() - startTime, TimeUnit.MILLISECONDS)
+                    metricById(it.id)?.requestMetric?.increment()
                 }
-                .doOnError() {
-                    metrics.errorMetric.increment() 
+                .doOnNext {
+                    handler.onResponse(it)
+                    metricById(it.id)?.callMetric?.record(System.currentTimeMillis() - startTime, TimeUnit.MILLISECONDS)
+                }
+                .doOnError {
+                    //when error happened the whole flux is stopped and no result is produced, so we should mark all the requests as failed
+                    call.items.forEach { item ->
+                        requestMetrics.get(chain, item.method).errorMetric.increment()
+                    }
                 }
                 .transform(writeRpcJson.toJsons(call))
         return if (call.type == ProxyCall.RpcType.SINGLE) {
@@ -170,15 +198,78 @@ class ProxyServer(
         }
     }
 
-    class RequestMetrics(chain: Chain, method: String) {
-        val callMetric = Timer.builder("request.jsonrpc.call")
-                .tags("chain", chain.chainCode, "eth_method", method)
+    interface RequestMetricsFactory {
+        fun get(chain: Chain, method: String): RequestMetrics
+    }
+
+    /**
+     * Standard metrics
+     */
+    class StandardRequestMetrics : RequestMetricsFactory {
+        private val current = EnumMap<Chain, RequestMetrics>(Chain::class.java)
+
+        override fun get(chain: Chain, method: String): RequestMetrics {
+            return current.getOrPut(chain) { RequestMetricsBasic(chain) }
+        }
+    }
+
+    /**
+     * Monitoring that has separate metrics per RPC method.
+     * Slightly slower to use than StandardRequestMetrics
+     */
+    class ExtendedRequestMetrics : RequestMetricsFactory {
+        private val current = HashMap<Ref, RequestMetrics>()
+        private val lock = ReentrantReadWriteLock()
+
+        override fun get(chain: Chain, method: String): RequestMetrics {
+            val ref = Ref(chain, method)
+            lock.read {
+                val existing = current[ref]
+                if (existing != null) {
+                    return existing
+                }
+            }
+            lock.write {
+                val existing = current[ref]
+                if (existing != null) {
+                    return existing
+                }
+                val created = RequestMetricsWithMethod(chain, method)
+                current[ref] = created
+                return created
+            }
+        }
+
+        data class Ref(val chain: Chain, val method: String)
+    }
+
+    interface RequestMetrics {
+        val callMetric: Timer
+        val errorMetric: Counter
+        val requestMetric: Counter
+    }
+
+    class RequestMetricsBasic(chain: Chain) : RequestMetrics {
+        override val callMetric = Timer.builder("request.jsonrpc.call")
+                .tags("chain", chain.chainCode)
                 .register(Metrics.globalRegistry)
-        val errorMetric = Counter.builder("request.jsonrpc.err")
-                .tags("chain", chain.chainCode, "eth_method", method)
+        override val errorMetric = Counter.builder("request.jsonrpc.err")
+                .tags("chain", chain.chainCode)
                 .register(Metrics.globalRegistry)
-        val requestMetric = Counter.builder("request.jsonrpc.request.total")
-                .tags("chain", chain.chainCode, "eth_method", method)
+        override val requestMetric = Counter.builder("request.jsonrpc.request.total")
+                .tags("chain", chain.chainCode)
+                .register(Metrics.globalRegistry)
+    }
+
+    class RequestMetricsWithMethod(chain: Chain, method: String) : RequestMetrics {
+        override val callMetric = Timer.builder("request.jsonrpc.call")
+                .tags("chain", chain.chainCode, "method", method)
+                .register(Metrics.globalRegistry)
+        override val errorMetric = Counter.builder("request.jsonrpc.err")
+                .tags("chain", chain.chainCode, "method", method)
+                .register(Metrics.globalRegistry)
+        override val requestMetric = Counter.builder("request.jsonrpc.request.total")
+                .tags("chain", chain.chainCode, "method", method)
                 .register(Metrics.globalRegistry)
     }
 }
