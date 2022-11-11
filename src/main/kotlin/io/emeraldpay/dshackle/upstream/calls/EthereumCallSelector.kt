a559416e9dd8dc82e11db061c317eeaef67708f9
a10zn8 <a1.cyb3r@gmail.com>
2022-11-11 17:48:35 +0400

    Added support of eth_uninstallFilter

--- src/main/kotlin/io/emeraldpay/dshackle/upstream/calls/EthereumCallSelector.kt
+++ src/main/kotlin/io/emeraldpay/dshackle/upstream/calls/EthereumCallSelector.kt
@@ -57,7 +57,7 @@ class EthereumCallSelector(
             return blockTagSelector(params, 1, head)
         } else if (method == "eth_getStorageAt") {
             return blockTagSelector(params, 2, head)
-        } else if (method == "eth_getFilterChanges") {
+        } else if (method == "eth_getFilterChanges" || method == "eth_uninstallFilter") {
             return sameUpstreamMatcher(params)
         }
         return Mono.empty()
