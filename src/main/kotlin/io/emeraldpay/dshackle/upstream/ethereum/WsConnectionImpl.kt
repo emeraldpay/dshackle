f4cacc123397fd4d6da32c996fc8a1c25bffd50d
Igor Artamonov <igor@artamonov.ru>
2022-11-16 22:33:03 -0500

    problem: timeout on unit tests (#203)

--- src/main/kotlin/io/emeraldpay/dshackle/upstream/ethereum/WsConnectionImpl.kt
+++ src/main/kotlin/io/emeraldpay/dshackle/upstream/ethereum/WsConnectionImpl.kt
@@ -331,7 +331,8 @@ open class WsConnectionImpl(
         )
 
         val response = Flux.from(getRpcResponses())
-            .doOnSubscribe { sendRpc(request) }
+            // send the request _after_ WS subscribes to the responses, otherwise the response may come before the actual subscription and be lost
+            .doOnRequest { sendRpc(request) }
             .filter { resp -> resp.id.asNumber() == expectedId }
             .take(Defaults.timeout)
             .take(1)
