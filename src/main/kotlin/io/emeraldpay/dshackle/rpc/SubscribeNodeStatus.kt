f94b320b79c9585fb1fa4114443ba9bfaed967f5
Termina1 <terminal2010@gmail.com>
2023-06-30 12:45:54 +0300

    reduce sample rate for chain heihgts

--- src/main/kotlin/io/emeraldpay/dshackle/rpc/SubscribeNodeStatus.kt
+++ src/main/kotlin/io/emeraldpay/dshackle/rpc/SubscribeNodeStatus.kt
@@ -103,7 +103,7 @@ class SubscribeNodeStatus(
     ): Flux<NodeStatusResponse> {
         val heads = upstream.getHead().getFlux()
             .takeUntilOther(cancel.asFlux())
-            .sample(Duration.ofMillis(1000))
+            .sample(Duration.ofMillis(200))
             .map { block ->
                 NodeStatusResponse.newBuilder()
                     .setDescription(
